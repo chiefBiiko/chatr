9ca81b7ac108f7dd245b0aee475d7dcef600f058
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-01-24 17:06:24 +0100

    corrected time checking

--- chatr.R
+++ chatr.R
@@ -9,12 +9,12 @@ library(tools)
 #
 # Usage:
 #   Get this repo on ur local machine
-#   Update the NAME variable below with your name
-#   setwd('PATH_2_chatr_DIRECTORY')
+#   Update the NAME variable below with ur name
+#   setwd('PATH_2_chatr_DIRECTORY')  # u can change dir once u have run chatrInit()
 #   source('chatr.R')
-#   chatrInit()  # start a background script that auto-pushes new msgs 2 your console
-#   chatr('hi')  # send a msg 2 your peers via the remote store
-#   chatrKill()  # stop background R process before exiting your R session!
+#   chatrInit()  # start a background script that auto-pushes new msgs 2 ur console
+#   chatr('hi')  # send a msg 2 ur peers via the remote store
+#   chatrKill()  # stop background R process before exiting ur R session!
 
 NAME <- 'Biiko'  # 'Balou', 'Christian'
 STORE_ID <- 'np94z'
@@ -29,14 +29,11 @@ chatrInit <- function(name=NAME, store_id=STORE_ID) {
   cbase$hash[[toTitleCase(name)]] <- T  # login
   cbase$msgs[[as.character(as.integer(Sys.time()))]] <- paste(format(Sys.time(), tz = 'UTC', '%d %b %H:%M'),
                                                               'UTC |', toTitleCase(name), 'logged in.')
-  if ((as.integer(Sys.time())-as.integer(names(cbase$msgs)[length(cbase$msgs)]))>21600) {  # 6h
+  if ((as.integer(Sys.time())-as.integer(names(cbase$msgs)[length(cbase$msgs)-1]))>21600) {  # 6h
     cbase$msgs <- cbase$msgs[length(cbase$msgs)]
-    res <- httr::PUT(paste0('http://api.myjson.com/bins/', store_id), body=cbase, encode='json')
-    if(httr::status_code(res)==200) message('Cleared old chat history and logged in.')
-  } else {
-    res <- httr::PUT(paste0('http://api.myjson.com/bins/', store_id), body=cbase, encode='json')
-    if(httr::status_code(res)==200) message('Logged in.')
   }
+  res <- httr::PUT(paste0('http://api.myjson.com/bins/', store_id), body=cbase, encode='json')
+  if (httr::status_code(res)==200) message('Logged in.')
   PID <<- sys::exec_background('Rscript', 'chatr_background.R', T, T)
   message(paste0('Started background R process with PID ', PID))
   message(paste('Online:', paste0(names(which(unlist(cbase$hash))), collapse=', '), '\nRun chatr("ur msg") 2 chat.'))
@@ -51,7 +48,7 @@ chatrKill <- function(pid=PID, name=NAME, store_id=STORE_ID) {
   xbase$msgs[[as.character(as.integer(Sys.time()))]] <- paste(format(Sys.time(), tz = 'UTC', '%d %b %H:%M'),
                                                               'UTC |', toTitleCase(name), 'logged out.')
   res <- httr::PUT(paste0('http://api.myjson.com/bins/', store_id), body=xbase, encode='json')
-  if(httr::status_code(res)==200) message('Logged out.')
+  if (httr::status_code(res)==200) message('Logged out.')
 }
 
 # Prepares and launches a PUT operation aka 'the chatting function'
