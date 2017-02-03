1d97bdc16ee54efe614e46542aa4cb80c542447c
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-02-03 09:53:48 +0100

    bettered notifications

--- chatr.R
+++ chatr.R
@@ -13,7 +13,7 @@
 #   chatrKill()  # stop background R process before exiting ur R session!
 
 lapply(list('devtools', 'sys', 'jsonlite', 'httr', 'tools'), function(p) {
-  if (!p%in%installed.packages()) install.packages(p)
+  if (!p %in% installed.packages()) install.packages(p)
 })
 
 library(sys)
@@ -28,17 +28,18 @@ message('Run chatrInit() 2 start chatting with your peers.')
 
 # Initializes a chat
 chatrInit <- function(name=NAME, store_id=STORE_ID) {
-  stopifnot(nchar(name)>0, nchar(store_id)>0, 'chatr.R'%in%list.files(), 'chatr_background.R'%in%list.files())
+  stopifnot(nchar(name) > 0, nchar(store_id) > 0,
+            'chatr.R' %in% list.files(), 'chatr_background.R' %in% list.files())
   message('Do not rm(PID, NAME, STORE_ID, chatr...) from your global environment!\nand\nPlease run chatrKill() before exiting your R session!')
   cbase <- jsonlite::fromJSON(paste0('http://api.myjson.com/bins/', store_id))
   cbase$hash[[toTitleCase(name)]] <- T  # login
   cbase$msgs[[as.character(as.integer(Sys.time()))]] <- paste(format(Sys.time(), tz='UTC', '%d %b %H:%M'),
                                                               'UTC |', toTitleCase(name), 'logged in.')
-  if ((as.integer(Sys.time())-as.integer(names(cbase$msgs)[length(cbase$msgs)-1]))>21600) {  # 6h
+  if ((as.integer(Sys.time()) - as.integer(names(cbase$msgs)[length(cbase$msgs) - 1])) > 21600) {  # 6h
     cbase$msgs <- cbase$msgs[length(cbase$msgs)]
   }
   res <- httr::PUT(paste0('http://api.myjson.com/bins/', store_id), body=cbase, encode='json')
-  if (httr::status_code(res)==200) message('Logged in.')
+  if (httr::status_code(res) == 200) message('Logged in.')
   PID <<- sys::exec_background('Rscript', 'chatr_background.R', T, T)
   message(paste0('Started background R process with PID ', PID))
   message(paste('Online:', paste0(names(which(unlist(cbase$hash))), collapse=', '), '\nRun chatr("ur msg") 2 chat.'))
@@ -53,15 +54,15 @@ chatrKill <- function(pid=PID, name=NAME, store_id=STORE_ID) {
   xbase$msgs[[as.character(as.integer(Sys.time()))]] <- paste(format(Sys.time(), tz='UTC', '%d %b %H:%M'),
                                                               'UTC |', toTitleCase(name), 'logged out.')
   res <- httr::PUT(paste0('http://api.myjson.com/bins/', store_id), body=xbase, encode='json')
-  if (httr::status_code(res)==200) message('Logged out.')
+  if (httr::status_code(res) == 200) message('Logged out.')
 }
 
 # Prepares and launches a PUT operation aka 'the chatting function'
 chatr <- function(msg, name=NAME, store_id=STORE_ID) {
-  stopifnot(typeof(msg)=='character', nchar(msg)>0)
+  stopifnot(typeof(msg) == 'character', nchar(msg) > 0)
   pbase <- jsonlite::fromJSON(paste0('http://api.myjson.com/bins/', store_id))
   pbase$msgs[[as.character(as.integer(Sys.time()))]] <- paste(format(Sys.time(), tz='UTC', '%d %b %H:%M'),
                                                               'UTC |', toTitleCase(name), ':', msg)
   res <- httr::PUT(paste0('http://api.myjson.com/bins/', store_id), body=pbase, encode='json')
-  if (httr::status_code(res)!=200) message(httr::status_code(res))
+  if (httr::status_code(res) != 200) message(httr::status_code(res))
 }
--- chatr_background.R
+++ chatr_background.R
@@ -1,31 +1,33 @@
 # chatr_background
 
 # 4 R & notifications c: github.com/gaborcsardi/notifier
-if (paste0(Sys.info()[1:2], collapse='')=='Windows7' && !file.exists('notifu/notifu.exe')) {
+if (paste0(Sys.info()[1:2], collapse='') == 'Windows7' && !file.exists('notifu/notifu.exe')) {
   message('Installing notifu.')  # if u r !using windows 7 check notifier repo 4 requirements
   temp <- tempfile()
   download.file('https://www.paralint.com/projects/notifu/dl/notifu-1.6.zip', temp)
   unzip(temp, exdir='notifu')
   unlink(temp)
 }
-if (!'notifier'%in%installed.packages()) source('https://install-github.me/gaborcsardi/notifier')
-if (!'beepr'%in%installed.packages()) devtools::install_github('rasmusab/beepr') 
+if (!'notifier' %in% installed.packages()) source('https://install-github.me/gaborcsardi/notifier')
+if (!'beepr' %in% installed.packages()) devtools::install_github('rasmusab/beepr') 
 
 library(jsonlite)
 library(notifier)
 library(beepr)
 
+NAME = 'Biiko'
 STORE_ID <- 'np94z'
 chatrbase <- list()
 
 # Gets a remote store and prints new messages to stdout pipe
-bgGET <- function(store_id=STORE_ID) {
-  stopifnot(nchar(store_id)>0)
+bgGET <- function(name=NAME, store_id=STORE_ID) {
+  stopifnot(nchar(store_id) > 0)
   chatrpast <- chatrbase
   chatrbase <<- jsonlite::fromJSON(paste0('http://api.myjson.com/bins/', store_id))$msgs
   new_msgs <- setdiff(chatrbase, chatrpast)
   lapply(new_msgs, cat, sep='\n')
-  if (length(new_msgs)>0) {
+  if (length(new_msgs) > 0 &&  # and checking that at least 1 msg is not ur own
+      !all(sapply(new_msgs, function(m) { sub('^.+\\|\\s(\\S+)\\s.+$', '\\1', m) == name }))) {
     try(notifier::notify(title='chatr', msg='New message'), T)
     try(beepr::beep(sample(c(2, 10), 1)), T)
   }
